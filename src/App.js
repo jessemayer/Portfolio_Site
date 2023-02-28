6c98bb6a491952b2004f9edea4458e3ec5c1c061
jessemayer1985@gmail <jessemayer1985@gmail.com>
2023-02-28 10:45:12 -0700

    Commit with animated Background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/NavBar';
+import AnimatedBackground from './components/AnimatedBackground';
 
 function App() {
   return (
-   <NavBar />
+    <>
+    <NavBar />
+    <AnimatedBackground />
+   </>
   )
 }
 
---
+++ src/components/AnimatedBackground.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+ class AnimatedBackground extends React.Component {
+  render() {
+    return (
+        <div className="context">
+        
+        <div className="area">
+          <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AnimatedBackground
